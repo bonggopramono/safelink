d870547a1492a7fbecb3d22f4fad3d474b477d35
bonggopramono <34598410+bonggopramono@users.noreply.github.com>
2020-04-11 22:35:44 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -19,12 +19,15 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*7)+ 1);
 var xxx=null;
 if(x=="1"){xxx="practice-environmental-awareness-to-our-surroundings.html"}
 if(x=="2"){xxx="learn-forex-trading-professionally.html"}
 if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="10-tips-for-start-small-business-startup.html"}a_to_ve[i].href="https://safelink.sekolahotomasi.com/2020/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="10-tips-for-start-small-business-startup.html"}
+if(x=="5"){xxx="is-it-true-lime-can-eliminate-acne.html"}
+if(x=="6"){xxx="the-way-to-start-business-at-home.html"}
+if(x=="7"){xxx="how-to-determine-health-insurance.html"}a_to_ve[i].href="https://safelink.sekolahotomasi.com/2020/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
